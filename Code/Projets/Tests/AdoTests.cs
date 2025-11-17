7164c162aab21cab4b22040b0d4d8b0d19235156
Dominique MAURAS <contact@msform.com>
2025-11-17 13:30:50 +0100

    Ado .net

---
+++ Code/Projets/Tests/AdoTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests;
+
+[TestClass]
+public sealed class AdoTests
+{
+
+    // Design pattern Generator / Iterator
+    // generator => il génère des éléments à la demande
+    IEnumerable<int> Entiers()
+    {
+        var i = 0;
+        while (i<int.MaxValue)
+        {
+            yield return i;
+            i++;
+        }
+    }
+
+
+    [TestMethod]
+    public void TestEnumerable()
+    {
+        var liste = SelectEmployes();
+        // Itéraotsur Entiers => Take(10)
+        foreach(var e in liste.Skip(1).Take(1))
+        {
+            var a = e;
+        }
+    }
+
+  
+    public IEnumerable<string> SelectEmployes()
+    {
+        // Très important : lorsque on utilise des objets qui utilisent des resources du système
+        // dans un bloc using
+        using (var conn = DI.Services.GetRequiredService<SqlConnection>())
+        {
+            conn.Open();
+            // Nécessité d'écrire la requète
+            // Qui peut être dépendante du logiciel de BDD utilisé
+            using (var requete = new SqlCommand("SELECT * FROM TBL_Employes", conn))
+            {
+                using (var reader = requete.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var nom = reader.GetString(3);
+                        yield return nom;
+                    }
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ Code/Projets/Tests/DI.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    internal class DI
+    {
+        public static readonly IServiceProvider Services;
+
+        static DI()
+        {
+            // Création de l'injection de dépendance
+            var serviceCollection = new ServiceCollection();
+
+            // Config basée sur fichier xml
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("appsettings.json");
+            var config = configBuilder.Build();
+            serviceCollection.AddSingleton<IConfiguration>(config);
+
+            serviceCollection.AddTransient<SqlConnection>(_ =>
+            {
+                var connectionString = config.GetConnectionString("MaSociete");
+                // Cette construction sera exécutée à chaque demande de service du type SqlConnection
+                return new SqlConnection(config.GetConnectionString("MaSociete"));
+            });
+
+
+
+            // ServiceProvider
+            Services = serviceCollection.BuildServiceProvider();
+            
+        }
+    }
+}
